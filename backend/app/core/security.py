4e4f289ea6f01acaf649c6d796d3cfc10d8396d6
Omagu Emmanuel <omagujr8@gmail.com>
2026-01-29 22:24:57 +0100

    Final Authentic and User Management changes

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,19 +5,23 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(plain: str, hashed: str ) -> str:
-    return pwd_context.hash(plain, hashed)
 
-def verify_password(plain: str, hashed: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain, hashed, hashed_password)
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: timedelta |None = None):
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire =  datetime.utcnow() + (expires_delta or timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.utcnow() + (
+        expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
     to_encode.update({"exp": expire})
     return jwt.encode(
         to_encode,
         settings.JWT_SECRET_KEY,
-        algorithm= settings.JWT_ALGORITHM
+        algorithm=settings.JWT_ALGORITHM,
     )
