4123cdd71d61cfc96a3dbce73c67e91eb15d4943
Omagu Emmanuel <omagujr8@gmail.com>
2026-02-19 16:18:20 +0100

    Registered Inventory router in router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1 import auth, categories, products
+from app.api.v1 import auth, categories, products, inventory
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(categories.router)
 api_router.include_router(products.router)
+api_router.include_router(inventory.router)
