59b00055ad409c75250cb33fc5c8636622087eb6
Omagu Emmanuel <omagujr8@gmail.com>
2026-03-10 19:44:41 +0100

    Initial commit on Order Management System

---
+++ backend/app/models/order.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    status = Column(String, default="CREATED")
+
+    total_amount = Column(Float, default=0)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    items = relationship(
+        "OrderItem",
+        back_populates="order",
+        cascade="all, delete"
+    )
