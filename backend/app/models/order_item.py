7017e1b3197035b8a2426b39a0cff23152ae24c0
Omagu Emmanuel <omagujr8@gmail.com>
2026-03-10 19:47:16 +0100

    Created order item

---
+++ backend/app/models/order_item.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+
+    variant_id = Column(Integer, ForeignKey("product_variants.id"), nullable=False)
+
+    quantity = Column(Integer, nullable=False)
+
+    price = Column(Float, nullable=False)
+
+    order = relationship("Order", back_populates="items")
