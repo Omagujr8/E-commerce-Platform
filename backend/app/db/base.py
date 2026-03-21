d75a053404ecb4a5c1d6db2dd43460de9d4775df
Omagu Emmanuel <omagujr8@gmail.com>
2026-03-21 11:44:24 +0100

    Registered payment model in db

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -7,6 +7,7 @@ from app.db.session import Base
 from app.models.user import User
 from app.models.product import Product
 from app.models.category import Category
+from app.models.payment import Payment
 
 #class Base(DeclarativeBase):
 #    pass
